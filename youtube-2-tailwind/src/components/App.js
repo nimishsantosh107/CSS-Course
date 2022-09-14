613f1a1e54ebd6053d2a8a28112789ae99f5590a
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2022-09-14 20:23:04 +0530

    tailwind course - flexbox, cards, badges, @apply directive

--- youtube-2-tailwind/src/components/App.js
+++ youtube-2-tailwind/src/components/App.js
@@ -10,10 +10,12 @@ const App = () => {
             </div>
             {/* NAVBAR END */}
             {/* MAIN */}
-            <main className="px-16 py-6">
-                <div>
+            <main className="px-16 py-6 bg-gray-200">
+                <div className="text-primary flex justify-center md:justify-end">
                     <a href="#">Log in</a>
-                    <a href="#">Sign Up</a>
+                    <a href="#" className="ml-2">
+                        Sign Up
+                    </a>
                 </div>
 
                 <header>
@@ -28,6 +30,8 @@ const App = () => {
                     <div>
                         {/* CARDS */}
                         <Card src="/image.png" />
+                        <Card src="/image.png" />
+                        <Card src="/image.png" />
                     </div>
 
                     <h4 className="font-bold mt-12 pb-2 border-b border-gray-300">
@@ -36,10 +40,14 @@ const App = () => {
                     <div>
                         {/* CARDS */}
                         <Card src="/image.png" />
+                        <Card src="/image.png" />
+                        <Card src="/image.png" />
                     </div>
 
-                    <div>
-                        <div>Load more...</div>
+                    <div className="flex justify-center">
+                        <div className="bg-secondary-100 text-white p-2 rounded-md hover:bg-secondary-200 transition">
+                            Load more...
+                        </div>
                     </div>
                 </div>
             </main>
--- youtube-2-tailwind/src/components/Card.js
+++ youtube-2-tailwind/src/components/Card.js
@@ -1,10 +1,15 @@
 const Card = ({ src }) => {
     return (
-        <div className="mt-8">
-            <img src={src} />
-            <div>
-                <span>Dish 1</span>
-                <span>Recipe by Bro</span>
+        <div className="card">
+            <img src={src} className="w-full h-32 sm:h-48 object-cover" />
+
+            <div className="badge">
+                <span>25 mins</span>
+            </div>
+
+            <div className="m-4">
+                <span className="font-bold">Dish 1</span>
+                <span className="block text-gray-500 text-sm font-extralight">Recipe by Bro</span>
             </div>
         </div>
     );
